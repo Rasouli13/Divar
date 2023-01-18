4992c95247fd71295b179bd7226a7ef6474b643c
Rasouli13 <93980465+Rasouli13@users.noreply.github.com>
2023-01-19 02:30:10 +0330

    Commit_2
    added setEmail, writeToFile;

--- Project/creatAccount.java
+++ Project/creatAccount.java
@@ -8,33 +8,32 @@ import java.util.regex.Pattern;
 import Project.Exceptions.*;
 
 public class creatAccount{
+    private String username, password, email;
 
     public void setUsername(String username) throws IOException, InvalidUsername {
         Matcher matcher = Pattern.compile("^\\w+$").matcher(username);
         if(!matcher.matches())
             throw new InvalidUsername("Invalid username!");
         File file = new File("users.txt");
-        FileWriter fw = new FileWriter(file,true);
-        if(!file.exists()){
-            file.createNewFile();
-            fw.write(username+ ":");
-            fw.flush();
-        }
-        else {
+        if(file.exists()) {
             Scanner scanner = new Scanner(file);
-            while (scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 String[] userPass = scanner.nextLine().split(":");
-                if(userPass[0].equals(username))
+                if (userPass[0].equals(username))
                     throw new InvalidUsername("This username already taken!");
             }
-            fw.write(username+ ":");
-            fw.flush();
         }
+        this.username = username;
     }
 
+    public void setEmail(String email) throws InvalidEmailAddress {
+        Matcher matcher = Pattern.compile("^[\\w\\-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$").matcher(email);
+        if(matcher.matches())
+            this.email = email;
+        else throw new InvalidEmailAddress();
+    }
 
     public void setPassword(String password) throws InvalidPassword, IOException {
-
         if (!checkSpelling(password))
             throw new InvalidPassword();
         if(binaryNotFound(password) && aIsNotEnough(password))
@@ -42,8 +41,25 @@ public class creatAccount{
         if( hasConsecutive(password))
             throw new InvalidPassword();
 
-
+        this.password = password;
     }
+
+    public void writeToFile() throws IOException {
+        File file = new File("users.txt");
+        FileWriter fw = new FileWriter(file,true);
+        if(!file.exists()){
+            file.createNewFile();
+            fw.write(this.username + ":" + this.password + ":" + this.email);
+            fw.flush();
+        }
+        else {
+            fw.write("\n" + this.username + ":" + this.password + ":" + this.email);
+            fw.flush();
+        }
+    }
+
+    //setPassword Methods---------------------------------------------------------------------------------------------------------------------
+
     public boolean checkSpelling(String password){
         Matcher matcher = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[a-z]).{8,}$").matcher(password);
         return matcher.matches();
@@ -101,7 +117,13 @@ public class creatAccount{
         int n = Integer.parseInt(s);
         return (n & n-1)==0;
     }
-    public static void main(String[] args) throws InvalidUsername, IOException, InvalidPassword {
 
+    //End setPassword Methods------------------------------------------------------------------------------------------------------------------------
+
+    public static void main(String[] args) throws InvalidUsername, IOException, InvalidPassword, InvalidEmailAddress {
+        creatAccount ca = new creatAccount();
+        ca.setUsername("abcd");
+        ca.setPassword("hdcbjs65656");
+        ca.setEmail("example.2@example.com");
     }
 }
