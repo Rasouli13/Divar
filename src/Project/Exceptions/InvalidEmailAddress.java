3100621720158f193ff147d5d382a3518951eea1
Rasouli13 <93980465+Rasouli13@users.noreply.github.com>
2023-01-24 04:33:35 +0330

    Delete InvalidEmailAddress.java

--- src/Project/Exceptions/InvalidEmailAddress.java
+++
@@ -1,7 +0,0 @@
-package Project.Exceptions;
-
-public class InvalidEmailAddress extends Exception{
-    public InvalidEmailAddress(){
-        super("Enter a valid Email: example.2@example.com");
-    }
-}
